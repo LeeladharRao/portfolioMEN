15f7e5c44a40b6eb30f627f0f5251a582b961a0b
LeeladharRao <kompallyleeladhar@gmail.com>
2021-09-26 20:35:18 +0530

    cleaning files

--- frontend-src/App.js
+++ frontend-src/App.js
@@ -1,10 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Route, Link, NavLink, Switch } from "react-router-dom"
-// import Home from "./components/Home"
-// import Animal from "./components/Animal"
-// import About from "./components/About"
-// import Secret from "./components/Secret"
 import Layout from "./components/Layout"
 import Blurb from "./components/Blurb"
 
