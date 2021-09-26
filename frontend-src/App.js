6c06ff6bd4d52b3185dc5f1a106aa1de6c13a1f7
LeeladharRao <kompallyleeladhar@gmail.com>
2021-09-26 20:18:34 +0530

    adding index

--- frontend-src/App.js
+++ frontend-src/App.js
@@ -1,45 +1,40 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Route, Link, NavLink, Switch } from "react-router-dom"
-import Home from "./components/Home"
-import Animal from "./components/Animal"
-import About from "./components/About"
-import Secret from "./components/Secret"
+// import Home from "./components/Home"
+// import Animal from "./components/Animal"
+// import About from "./components/About"
+// import Secret from "./components/Secret"
 import Layout from "./components/Layout"
+import Blurb from "./components/Blurb"
 
 function App() {
+
+  const Section = ({ title, children, button, ...props }) => (
+    <section {...props}>
+      <h2 className="section-title">
+        {title}
+        {button && (
+          <Link className="section-button" to="/blog">
+            View all
+          </Link>
+        )}
+      </h2>
+      {children}
+    </section>
+  )
+
   return (
     <BrowserRouter>
-      <Layout />
-
-      <div className="container py-3 py-md-5">
-        <Switch>
-          <Route path="/animal/:name">
-            <Animal />
-          </Route>
-          <Route path="/secret">
-            <Secret />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-
-      <footer className="border-top pt-3">
-        <p className="small text-center text-muted">
-          <Link className="text-muted mr-1" to="/">
-            Home
-          </Link>{" "}
-          |{" "}
-          <Link className="text-muted ml-1" to="/about">
-            About Us
-          </Link>
-        </p>
-      </footer>
+      <Layout>
+        <Blurb title="I'm Tania Rascia.">
+          <p>
+            I'm a software engineer and writer. This website is my digital garden
+            &mdash; a compendium of the things I've learned and created over the
+            years.
+          </p>
+        </Blurb>
+      </Layout>
     </BrowserRouter>
   )
 }
