be824f2aba857fa8e0bcdba3c95952305710fb36
LeeladharRao <kompallyleeladhar@gmail.com>
2021-09-26 20:24:46 +0530

    webpack changed

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       }, {
-        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        test: /\.(png|jpg|gif)$/i,
         use: [
           {
             loader: 'url-loader',
