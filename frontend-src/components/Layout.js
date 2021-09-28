4037f41f4acc0da9ac1980789e870a99e706c60b
LeeladharRao <kompallyleeladhar@gmail.com>
2021-09-28 22:42:55 +0530

    import

--- frontend-src/components/Layout.js
+++ frontend-src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from './Nav'
 import Footer from './Footer'
 import App from '../pages/index'
+import { BrowserRouter, Route, Link, NavLink, Switch } from "react-router-dom"
 
 export default function Layout({ children }) {
   return (
