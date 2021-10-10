b0c4b3a7177bc60a46d0306248554e7982c8a615
LeeladharRao <kompallyleeladhar@gmail.com>
2021-10-10 21:01:29 +0530

    changes to class

--- frontend-src/components/Posts.js
+++ frontend-src/components/Posts.js
@@ -4,30 +4,20 @@ import React from 'react'
 export default function Posts() {
 
 
-  const Cell = () => {
-
-    return (
-      <div className="post" >
-
-          <div className="post-row">
-            <time>Jan 04</time>
-            <h3>title</h3>
-          </div>
-
-      </div>
-
-
-    )
-  }
-
-
   return (
     <section>
       <section>
         <h2>2021</h2>
-        <div className="posts">
-          <Cell />
-          <Cell />
+        <div class="posts">
+
+          <div class="post" >
+
+            <div class="post-row">
+              <time>Jan 04</time>
+              <h3>title</h3>
+            </div>
+
+          </div>
         </div>
       </section>
     </section>
--- frontend-src/pages/blog.js
+++ frontend-src/pages/blog.js
@@ -20,7 +20,7 @@ export default function BlogIndex() {
         </div>
       </header>
       <section>
-        <div className="container">
+        <div class="container">
           <Posts />
         </div>
       </section>
