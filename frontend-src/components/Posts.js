9cab2b665a136df2b6c818a9329438082b4e45ca
LeeladharRao <kompallyleeladhar@gmail.com>
2021-10-10 21:21:50 +0530

    testing posts

--- frontend-src/components/Posts.js
+++ frontend-src/components/Posts.js
@@ -9,14 +9,23 @@ export default function Posts() {
       <section>
         <h2>2021</h2>
         <div class="posts">
-
           <div class="post" >
-
             <div class="post-row" href="#">
               <time>Jan 04</time>
               <h3>title</h3>
             </div>
-
+          </div>
+          <div class="post" >
+            <div class="post-row" href="#">
+              <time>Jan 04</time>
+              <h3>title</h3>
+            </div>
+          </div>
+          <div class="post" >
+            <div class="post-row" href="#">
+              <time>Jan 04</time>
+              <h3>title</h3>
+            </div>
           </div>
         </div>
       </section>
