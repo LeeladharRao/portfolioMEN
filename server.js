ae2159740d191ad8d4b827ed58d80d06dd27f33d
LeeladharRao <kompallyleeladhar@gmail.com>
2021-10-11 21:35:42 +0530

    testing url

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 
-const url = "mongodb+srv://Admin:B$BVgxgr123@portfoliocluster.y45fo.mongodb.net/postsDB";
-mongoose.connect(url);
+mongoose.connect("mongodb+srv://Admin:B$BVgxgr123@portfoliocluster.y45fo.mongodb.net/postsDB");
 
 app.use("/", require("./frontend-src/components/postRoute"))
 
